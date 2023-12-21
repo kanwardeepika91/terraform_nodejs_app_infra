ca9dcb1a420604a5dc72a337af05064410ea8737
deepika.kanwar <kanwardeepika91@gmail.com>
2023-12-21 07:33:02 +0530

    final code

--- modules/2.ecs/0.ecr.tf
+++ modules/2.ecs/0.ecr.tf
@@ -1,6 +1,8 @@
 resource "aws_ecr_repository" "ecr_repo_for_ecs" {
   name = "${var.env}-ecr"
   image_tag_mutability = "MUTABLE" # to put the latest tag on most recent image
+  force_delete = true #### isnot working. use below command to delete manually
+  #aws ecr delete-repository --repository-name dev-ecs-ecr --force --region us-east-1
   image_scanning_configuration {
   scan_on_push = true
   }
